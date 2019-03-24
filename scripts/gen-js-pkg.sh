35902d97c7c0e57076d1bc2eaaad7af65e4ceb7c
Robert Lin <robertlin1@gmail.com>
2019-03-24 10:38:00 -0700

    py, js: move generated code to /pb

--- scripts/gen-js-pkg.sh
+++ scripts/gen-js-pkg.sh
@@ -6,7 +6,7 @@ echo "[INFO] generating js bindings for '$PKG'..."
 mkdir -p js/temporal/src
 protoc -I=proto \
   -I"$GOPATH"/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --js_out=import_style=commonjs:js/temporal/src \
+  --js_out=import_style=commonjs:js/temporal/src/pb \
   --plugin=protoc-gen-grpc-web=build/protoc-gen-grpc-web \
-  --grpc-web_out=import_style=commonjs,mode=grpcwebtext:js/temporal/src \
+  --grpc-web_out=import_style=commonjs,mode=grpcwebtext:js/temporal/src/pb \
   "$PKG.proto"
--- scripts/gen-py-pkg.sh
+++ scripts/gen-py-pkg.sh
@@ -5,6 +5,6 @@ PKG=$1
 python3 -m grpc_tools.protoc \
   -Iproto \
   -I"$GOPATH"/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --python_out=py/temporal \
-  --grpc_python_out=py/temporal \
+  --python_out=py/temporal/pb \
+  --grpc_python_out=py/temporal/pb \
   "$PKG.proto"
